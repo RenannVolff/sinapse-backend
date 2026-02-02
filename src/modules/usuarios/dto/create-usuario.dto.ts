a5b339385757db61fa4c64b1b4f84f637ac72c17
Renann Felipe Wolff <141967574+WolffShadow@users.noreply.github.com>
2026-02-02 14:48:16 -0300

    feat: Config. das DTO para Validações e Adição do Mód. Auth

--- src/modules/usuarios/dto/create-usuario.dto.ts
+++ src/modules/usuarios/dto/create-usuario.dto.ts
@@ -1,5 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 export class CreateUsuarioDto {
+  @ApiProperty({ description: 'Nome completo do profissional' })
+  @IsString()
+  @IsNotEmpty({ message: 'O nome é obrigatório' })
   nome: string;
+
+  @ApiProperty({ description: 'E-mail para login no sistema' })
+  @IsEmail({}, { message: 'Forneça um e-mail válido' })
+  @IsNotEmpty()
   email: string;
+
+  @ApiProperty({ description: 'Senha de acesso (mínimo 6 caracteres)' })
+  @IsString()
+  @MinLength(6, { message: 'A senha deve ter no mínimo 6 caracteres' })
   senha: string;
 }
